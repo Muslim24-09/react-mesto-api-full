70945a5eaedb324b892bae2d3906296762a17901
Muslim24-09 <Leonard76@mail.ru>
2023-02-05 18:55:15 +0300

    restore backend

---
+++ backend/express-mesto-gha/controllers/cards.js
@@ -0,0 +1,97 @@
+const Card = require('../models/card');
+
+const BadRequestError = require('../errors/BadRequestError');
+const NotFoundError = require('../errors/NotFoundError');
+const ForbiddenError = require('../errors/ForbiddenError');
+
+const getCards = (_, res, next) => {
+  Card.find({})
+    .then((cards) => {
+      res.status(200).send({ data: cards });
+    })
+    .catch((err) => next(err));
+};
+
+const deleteCardById = (req, res, next) => {
+  Card.findById(req.params.cardId)
+    .then((card) => {
+      if (!card) {
+        throw new NotFoundError('Запрашиваемая карточка не найдена');
+      }
+      if (!card.owner.equals(req.user._id)) {
+        throw new ForbiddenError('Вы можете удалить только свою карточку!');
+      }
+      return card.remove().then(() => res.status(200).send({ data: card }));
+    })
+    .catch((err) => {
+      if (err.name === 'CastError') {
+        next(new BadRequestError('Переданы некорректные данные'));
+      } else {
+        next(err);
+      }
+    });
+};
+
+const createCard = (req, res, next) => {
+  const { name, link } = req.body;
+  Card.create({
+    name, link, owner: req.user._id,
+  })
+    .then((card) => res.status(200).send({ data: card }))
+    .catch((err) => {
+      if (err.name === 'ValidationError') {
+        next(new BadRequestError('Переданы некорректные данные'));
+      } else {
+        next(err);
+      }
+    });
+};
+
+const likeCard = (req, res, next) => {
+  Card.findByIdAndUpdate(
+    req.params.cardId,
+    { $addToSet: { likes: req.user._id } }, // добавить _id в массив, если его там нет
+    { new: true },
+  )
+    .then((card) => {
+      if (!card) {
+        throw new NotFoundError('Запрашиваемая карточка не найдена');
+      }
+      res.status(200).send({ data: card });
+    })
+    .catch((err) => {
+      if (err.name === 'CastError') {
+        next(new BadRequestError('Переданы некорректные данные'));
+      } else {
+        next(err);
+      }
+    });
+};
+
+const dislikeCard = (req, res, next) => {
+  Card.findByIdAndUpdate(
+    req.params.cardId,
+    { $pull: { likes: req.user._id } }, // убрать _id из массива
+    { new: true },
+  )
+    .then((card) => {
+      if (!card) {
+        throw new NotFoundError('Запрашиваемая карточка не найдена');
+      } else res.status(200).send({ data: card });
+    })
+    .catch((err) => {
+      if (err.name === 'CastError') {
+        next(new BadRequestError('Переданы некорректные данные'));
+      } else {
+        next(err);
+      }
+    });
+};
+
+module.exports = {
+  getCards,
+  deleteCardById,
+  createCard,
+  likeCard,
+  dislikeCard,
+};
---
+++ backend/express-mesto-gha/routes/users.js
@@ -0,0 +1,35 @@
+const router = require('express').Router(); // создали роутер
+const { celebrate, Joi } = require('celebrate');
+
+const regExp = require('../constants/constants');
+
+const {
+  getUsers,
+  getUserById,
+  getCurrentUser,
+  updateUser,
+  updateAvatar,
+} = require('../controllers/users');
+
+router.get('/', getUsers);
+router.get('/me', getCurrentUser);
+router.get('/:userId', celebrate({
+  params: Joi.object().keys({
+    userId: Joi.string().length(24).hex().required(),
+  }),
+}), getUserById);
+
+router.patch('/me', celebrate({
+  body: Joi.object().keys({
+    name: Joi.string().min(2).max(30).required(),
+    about: Joi.string().min(2).max(30).required(),
+  }),
+}), updateUser);
+
+router.patch('/me/avatar', celebrate({
+  body: Joi.object().keys({
+    avatar: Joi.string().pattern(regExp).required(),
+  }),
+}), updateAvatar);
+
+module.exports = router;
